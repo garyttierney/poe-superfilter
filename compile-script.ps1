087d17d7c7609778eb3a22b111d7304a3d27b667
Stefan Kaufhold <cere@fastmail.fm>
2017-04-19 03:22:54 +0200

    update compile script and installer

--- compile-script.ps1
+++ compile-script.ps1
@@ -1,25 +1,16 @@
-﻿$version = "v0.1.0"
+﻿$version = "v0.1.1"
 $input_file = "examples/full_filter.sf"
 $output_file = "examples/full_filter.filter"
 
-
-$sf_dir = $env:APPDATA + "\superfilter"
+$sf_dir = ${env:ProgramFiles(x86)} + "\Superfilter\" + $version
 $sf_dir_exists = Test-Path -Path $sf_dir -PathType Container
 if (!$sf_dir_exists) {
-    Write-Host "Creating Superfilter directory..."
-    New-Item -ItemType Directory -Force -Path $sf_dir
+    Write-Host "The requested superfilter version is not installed."
+    Write-Host "Please visit https://github.com/skaufhold/poe-superfilter/releases and download $version"
+} else {
+    $sf_bin = $sf_dir + "\superfilter.exe"
+    & $sf_bin $input_file --output="$output_file" --pretty
 }
-
-$sf_bin = $sf_dir + "/superfilter_" + $version + ".exe"
-$sf_bin_exists = Test-Path -Path $sf_bin -PathType Leaf
-
-if (!$sf_bin_exists) {
-    $bin_uri = "https://github.com/skaufhold/poe-superfilter/releases/download/" + $version + "/superfilter.exe"
-    Write-Host "Superfilter binary not found, downloading from " + $bin_uri
-    Invoke-WebRequest -Uri $bin_uri -OutFile $sf_bin
-}
-
-& $sf_bin $input_file --output="$output_file" --pretty
 
 Write-Host "Press any key to exit"
 
