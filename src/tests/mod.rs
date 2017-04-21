ae0e9a6511c5314084d9ca408c166facb165a276
Stefan Kaufhold <cere@fastmail.fm>
2017-04-21 05:07:04 +0200

    add testing setup

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,16 +1,42 @@
 
-use typed_arena::Arena;
+use std::path::Path;
+use std::io::Read;
+use std::fs::File;
+use super::ast::transform::RenderConfig;
 
-use super::*;
+fn load_example(file_name : &str) -> String {
+    let path = "src/tests/".to_owned() + file_name;
+    println!("{}", path);
+    let mut file = File::open(path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    contents
+}
+
+fn test_compile(input_file : &str, expected_output_file : &str) {
+    let sf_file = load_example(input_file);
+    let expected_result = load_example(expected_output_file);
+
+    let mut result_vec = Vec::<u8>::new();
+
+    let base_path = Path::new(&input_file)
+        .parent()
+        .unwrap()
+        .to_owned();
+
+    let render_config = RenderConfig {
+        pretty: false,
+        indent_str: "    ",
+        base_path: base_path
+    };
+
+    super::compile(&sf_file, input_file.to_owned(), &mut result_vec, &render_config).unwrap();
+
+    let result_str = String::from_utf8(result_vec).unwrap();
+    assert!(expected_result.eq(&result_str))
+}
 
 #[test]
-fn basic_vars() {
-    let sfilter = indoc!(
-    "Show
-        Class Flask
-        Rarity Magic");
-
-    let tokens = Box::new(tok::tokenize(sfilter));
-    let ast_arena = Arena::new();
-    let filter = filter::parse_Filter(&ast_arena, tokens.into_iter());
+fn test_vars() {
+    test_compile("vars.sf", "vars.filter")
 }
